e9051de93793f0373139666b83ed5ab899b70483
Derek Gliwa <90353098+derekgliwa@users.noreply.github.com>
2023-12-05 22:30:39 -0800

    Fail at part 2

--- dec5/part2.py
+++ dec5/part2.py
@@ -4,9 +4,10 @@ def parse_mappings():
   input_file = open('input.txt', 'r')
   input = input_file.read()
   lines = input.strip().split('\n\n')
-  seeds = map(lambda x: int(x), re.split('\s', lines[0])[1:])
+  seeds = list(map(lambda x: int(x), re.split('\s', lines[0])[1:]))
+  seeds_with_ranges = zip(seeds[::2], seeds[1::2])
   maps_in_order = map(lambda x: parse_mapping(x), lines[1:])
-  return { 'seeds': seeds, 'maps_in_order': maps_in_order }
+  return { 'seeds': seeds_with_ranges, 'maps_in_order': maps_in_order }
 
 def parse_mapping(mapping):
   mappings = list(map(lambda x: calculate_mapping_row(x), re.split('\n', mapping)[1:]))
@@ -25,12 +26,15 @@ if __name__ == '__main__':
   mappings = parse_mappings()
   final_locations = []
   mappings_in_order = list(mappings['maps_in_order'])
-  for seed in list(mappings['seeds']):
-    start = seed
-    for sorted_mappings in mappings_in_order:
-      for mapping in sorted_mappings:
-        if start >= mapping['source'] and start < mapping['source'] + mapping['count']:
-          start = start + (mapping['destination'] - mapping['source'])
-          break
-    final_locations.append(start)
+  for seed, seed_range in list(mappings['seeds']):
+    for i in range(seed, seed + seed_range):
+      print(i)
+      start = i
+      for sorted_mappings in mappings_in_order:
+        for mapping in sorted_mappings:
+          if start >= mapping['source'] and start < mapping['source'] + mapping['count']:
+            start = start + (mapping['destination'] - mapping['source'])
+            break
+      final_locations.append(start)
   print(min(final_locations))
+
