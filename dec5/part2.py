f5ce20d707fd8ae8b96d0516bd04d99e70ee5a83
Derek Gliwa <90353098+derekgliwa@users.noreply.github.com>
2023-12-06 08:36:45 -0800

    Still failing

--- dec5/part2.py
+++ dec5/part2.py
@@ -21,15 +21,45 @@ def calculate_mapping_row(row):
     'count': int(count)
   }
 
+def find_seed_options(seed_range, mappings):
+  min_seed = seed_range[0]
+  max_seed = seed_range[1]
+  if len(mappings) == 0:
+    return { min_seed }
+
+  seed_options = set()
+  for mapping in mappings[0]:
+    if min_seed >= mapping['source'] and max_seed < mapping['source'] + mapping['count']:
+      seed_options.add(min_seed)
+      return seed_options
+    elif min_seed >= mapping['source'] and max_seed >= mapping['source'] + mapping['count']:
+      seed_options.add(min_seed)
+      min_seed = mapping['source'] + mapping['count']
+    elif min_seed < mapping['source']:
+      if max_seed < mapping['source']:
+        return seed_options.union(find_seed_options((min_seed, max_seed), mappings[1:]))
+      elif max_seed >= mapping['source'] and max_seed < mapping['source'] + mapping['count']:
+        seed_options.union(find_seed_options(min_seed, mapping['source'] - 1), mappings[1:])
+        seed_options.add(mapping['source'])
+      elif max_seed >= mapping['source'] and max_seed >= mapping['source'] + mapping['count']:
+        seed_options.union(find_seed_options(min_seed, mapping['source'] - 1), mappings[1:])
+        seed_options.add(mapping['source'])
+        min_seed = mapping['source'] + mapping['count']
+  if min_seed != max_seed:
+    seed_options.add(min_seed)
+  return seed_options
 
 if __name__ == '__main__':
   mappings = parse_mappings()
   final_locations = []
   mappings_in_order = list(mappings['maps_in_order'])
   for seed, seed_range in list(mappings['seeds']):
-    for i in range(seed, seed + seed_range):
-      print(i)
-      start = i
+    seed_options = find_seed_options((seed, seed + seed_range - 1), mappings_in_order)
+    print('seed_options')
+    print(seed_options)
+    for seed in seed_options:
+      print(seed)
+      start = seed
       for sorted_mappings in mappings_in_order:
         for mapping in sorted_mappings:
           if start >= mapping['source'] and start < mapping['source'] + mapping['count']:
